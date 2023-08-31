77c547cc90f464dc91d33a1625733b54cc230673
wiburga <wiburga@espe.edu.ec>
2023-08-31 07:59:04 -0500

    algoritmo

--- frontend/src/components/Party.jsx
+++ frontend/src/components/Party.jsx
@@ -1,15 +1,85 @@
-export const Party = ({ name, vote, key }) => {
+import React, { useEffect, useState } from "react";
+
+
+const apiURL = import.meta.env.VITE_API;
+
+
+
+const divideHighestByTwo = (votes) => {
+  const highestVote = Math.max(...votes);
+  return votes.map(vote => (vote === highestVote) ? Math.floor(vote / 2) : vote);
+};
+
+export const Party = ({ provinceId, numColumns }) => {
+
+  console.log(numColumns);
+  
+  const [suffrages, setSuffrages] = useState([]);
+
+  useEffect(() => {
+    const fetchSuffrages = async () => {
+      try {
+        const response = await fetch(`${apiURL}suffrage/${provinceId}`);
+        const data = await response.json();
+        setSuffrages(data);
+        
+      } catch (error) {
+        console.error("Error al obtener los votos:", error);
+      }
+    };
+
+    fetchSuffrages();
+  }, [provinceId]);
+
+  const createColumns = () => {
+    const columns = [];
+    let prevColumn = suffrages.map(s => s.SUF_VOTE);
+
+    for (let i = 0; i < numColumns; i++) {
+      const column = (i === 0) ? prevColumn : divideHighestByTwo(prevColumn);
+      columns.push(column);
+      prevColumn = column;
+    }
+
+    return columns;
+  };
+
+  const columns = createColumns();
+
   return (
-    <section className="Party" key={key}>
-      <section className="Party-name">
-        <h2>{name}</h2>
-      </section>
-      <section className="Party-vote">
-        <p className="Party-p">
-          Votos
-          <span className="Party-vote">{vote}</span>
-        </p>
-      </section>
+    <section className="Home-partys">
+      <div className="Grid-container">
+      <div className="Grid-header">
+          {/*<div className="Grid-cell header">Nombre de la Lista</div>
+          <div className="Grid-cell header">Votos</div>
+          {Array.from({ length: numColumns }, (_, i) => (
+            <div className="Grid-cell header" key={i}>
+              Columna {i + 1}
+            </div>
+          ))}*/}
+        </div>
+        {suffrages.map((suffrage, rowIndex) => (
+          <div className="Grid-row" key={rowIndex}>
+            <div className="Grid-cell">
+              {suffrage.PAR_NAME}
+            </div>
+            <div className="Grid-cell">
+              Votos:
+              {suffrage.SUF_VOTE}
+            </div>
+            {columns.map((column, columnIndex) => {
+              const cellValue = column[rowIndex];
+              const maxValue = Math.max(...column);
+              const isMaxValue = cellValue === maxValue;
+              return (
+                <div className={`Grid-cell ${isMaxValue ? "max-value" : ""}`} key={columnIndex}>
+                  {cellValue}
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
